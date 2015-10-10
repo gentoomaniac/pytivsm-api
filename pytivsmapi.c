aed69f169b7c2c61e29d35c8e679baacab98ec82
Marco Siebecke <marco@spotify.com>
2015-10-10 18:31:26 +0200

    added dsmTerminate and  more error codes

--- pytivsmapi.c
+++ pytivsmapi.c
@@ -16,9 +16,42 @@ void setError(int rc) {
         case DSM_RC_ACCESS_DENIED:
             sprintf(errorMessage, "API Error: DSM_RC_ACCESS_DENIED (%i)", rc);
             break;
+
+        case DSM_RC_BAD_HOST_NAME:
+            sprintf(errorMessage, "API Error: DSM_RC_BAD_HOST_NAME (%i)", rc); break;
+
+        case DSM_RC_REJECT_NO_RESOURCES:
+            sprintf(errorMessage, "API Error: DSM_RC_REJECT_NO_RESOURCES (%i)", rc); break;
+
+        case DSM_RC_INVALID_OPT:
+            sprintf(errorMessage, "API Error: DSM_RC_INVALID_OPT (%i)", rc); break;
+        case DSM_RC_NO_HOST_ADDR:
+            sprintf(errorMessage, "API Error: DSM_RC_NO_HOST_ADDR (%i)", rc); break;
         case DSM_RC_NO_OPT_FILE:
-            sprintf(errorMessage, "API Error: DSM_RC_NO_OPT_FILE (%i)", rc);
-            break;
+            sprintf(errorMessage, "API Error: DSM_RC_NO_OPT_FILE (%i)", rc); break;
+        case DSM_RC_MACHINE_SAME:
+            sprintf(errorMessage, "API Error: DSM_RC_MACHINE_SAME (%i)", rc); break;
+        case DSM_RC_INVALID_SERVER:
+            sprintf(errorMessage, "API Error: DSM_RC_INVALID_SERVER (%i)", rc); break;
+        case DSM_RC_INVALID_KEYWORD:
+            sprintf(errorMessage, "API Error: DSM_RC_INVALID_KEYWORD (%i)", rc); break;
+        case DSM_RC_PATTERN_TOO_COMPLEX:
+            sprintf(errorMessage, "API Error: DSM_RC_PATTERN_TOO_COMPLEX (%i)", rc); break;
+        case DSM_RC_NO_CLOSING_BRACKET:
+            sprintf(errorMessage, "API Error: DSM_RC_NO_CLOSING_BRACKET (%i)", rc); break;
+        case DSM_RC_NLS_CANT_OPEN_TXT:
+            sprintf(errorMessage, "API Error: DSM_RC_NLS_CANT_OPEN_TXT (%i)", rc); break;
+        case DSM_RC_NLS_INVALID_CNTL_REC:
+            sprintf(errorMessage, "API Error: DSM_RC_NLS_INVALID_CNTL_REC (%i)", rc); break;
+        case DSM_RC_NOT_ADSM_AUTHORIZED:
+            sprintf(errorMessage, "API Error: DSM_RC_NOT_ADSM_AUTHORIZED (%i)", rc); break;
+        case DSM_RC_NO_INCLEXCL_FILE:
+            sprintf(errorMessage, "API Error: DSM_RC_NO_INCLEXCL_FILE (%i)", rc); break;
+        case DSM_RC_NO_SYS_OR_INCLEXCL:
+            sprintf(errorMessage, "API Error: DSM_RC_NO_SYS_OR_INCLEXCL (%i)", rc); break;
+
+        case DSM_RC_NO_NODE_REQD:
+            sprintf(errorMessage, "API Error: DSM_RC_NO_NODE_REQD (%i)", rc); break;
         default:
             sprintf(errorMessage, "API Error: %i", rc);
     }
@@ -28,15 +61,15 @@ void setError(int rc) {
 
 static PyObject * dsmInit_wrapper(PyObject * self, PyObject * args, PyObject * keywds)
 {
-    dsUint32_t dsmHandle;;
+    dsUint32_t dsmHandle;
     dsmApiVersion dsmApiVersion;
     PyObject * apiVersion = NULL;
-    char* clientNodeName;
-    char* clientOwnerName;
-    char* clientPassword;
-    char* applicationType;
-    char* configfile;
-    char* options;
+    char* clientNodeName = NULL;
+    char* clientOwnerName = NULL;
+    char* clientPassword = NULL;
+    char* applicationType = NULL;
+    char* configfile = NULL;
+    char* options = NULL;
 
     int rc;
 
@@ -44,7 +77,7 @@ static PyObject * dsmInit_wrapper(PyObject * self, PyObject * args, PyObject * k
         "applicationType", "configfile", "options"};
 
     // parse arguments
-    if (!PyArg_ParseTupleAndKeywords(args, keywds, "Ossssss", kwlist, &apiVersion, &clientNodeName, &clientOwnerName,
+    if (!PyArg_ParseTupleAndKeywords(args, keywds, "O|ssssss", kwlist, &apiVersion, &clientNodeName, &clientOwnerName,
         &clientPassword, &applicationType, &configfile, &options)) {
     return NULL;
     }
@@ -52,18 +85,14 @@ static PyObject * dsmInit_wrapper(PyObject * self, PyObject * args, PyObject * k
     dsmApiVersion.release = PyInt_AsLong(PyDict_GetItemString(apiVersion, "release"));
     dsmApiVersion.level = PyInt_AsLong(PyDict_GetItemString(apiVersion, "level"));
 
-    rc = dsmInit(&dsmHandle, &dsmApiVersion, clientNodeName, NULL, clientPassword,
-            applicationType, NULL, NULL);
-
-    // TODO: pass actual values or NULL if empty strings
-    //rc = dsmInit(dsmHandle, &dsmApiVersion, clientNodeName, clientOwnerName, clientPassword,
-    //        applicationType, configfile, options);
+    rc = dsmInit(&dsmHandle, &dsmApiVersion, clientNodeName, clientOwnerName, clientPassword,
+            applicationType, configfile, options);
     if(rc) {
         setError(rc);
         return NULL;
     }
 
-    return Py_BuildValue("i", dsmHandle);
+    return Py_BuildValue("K", dsmHandle);
 }
 
 // http://www-304.ibm.com/support/knowledgecenter/SSGSG7_7.1.0/com.ibm.itsm.client.develop.doc/r_cmd_dsmqueryapiversion.html
@@ -130,11 +159,32 @@ static void dsmSetUp_wrapper(PyObject * self, PyObject * args, PyObject * keywds
     }
 }
 
+static void dsmTerminate_wrapper(PyObject * self, PyObject * args, PyObject * keywds)
+{
+    dsUint32_t dsmHandle;
+    int rc;
+
+    static char *kwlist[] = {"dsmHandle"};
+
+    // parse arguments
+    if (!PyArg_ParseTupleAndKeywords(args, keywds, "K", kwlist, &dsmHandle)) {
+    return;
+    }
+
+    rc = dsmTerminate(dsmHandle);
+
+    if(rc) {
+        setError(rc);
+        return;
+    }
+}
+
 static PyMethodDef TIVsmAPIMethods[] = {
-     { "dsmInit", (PyCFunction)dsmInit_wrapper, METH_KEYWORDS, "Init TSM environment" },
+     { "dsmInit", (PyCFunction)dsmInit_wrapper, METH_KEYWORDS, "Init TSM session" },
      { "dsmQueryApiVersion", dsmQueryApiVersion_wrapper, METH_NOARGS, "Query TSM API version" },
      { "dsmQueryApiVersionEx", dsmQueryApiVersionEx_wrapper, METH_NOARGS, "Query TSM API version" },
      { "dsmSetUp", (PyCFunction)dsmSetUp_wrapper, METH_KEYWORDS, "Set up TSM environment" },
+     { "dsmTerminate", (PyCFunction)dsmTerminate_wrapper, METH_KEYWORDS, "Terminate TSM session" },
       { NULL, NULL, 0, NULL }
 };
 
